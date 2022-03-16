aae0a1e0c7bb8aa2d078e3edfe817a0910597f77
ReitoSerizawa <s.reitiger@gmail.com>
2022-03-16 18:38:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let emptyArray = []
+    for (let i = 0; i < name.length; i++){
+        emptyArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return emptyArray
+}
+
+function countDown(num){
+    let i = num
+    while (i >= 0){
+        console.log(i)
+        i--
+    }
+}
