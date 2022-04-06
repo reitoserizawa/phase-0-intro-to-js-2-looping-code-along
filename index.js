03d24a91a292eba3355c3a89d1e910277fa9be71
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-06 13:20:47 -0400

    commit tree 176c84c07ce721628bcc7fc4438dee3be86515c8

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-function writeCards(name, event){
-    let emptyArray = []
-    for (let i = 0; i < name.length; i++){
-        emptyArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+function writeCards(arrayStrings, eventName) {
+    let newArrays = []
+
+    for (let i = 0; i <arrayStrings.length; i++) {
+        newArrays.push(`Thank you, ${arrayStrings[i]}, for the wonderful ${eventName} gift!`)
     }
-    return emptyArray
+
+    return newArrays
+ 
 }
 
-function countDown(num){
-    let i = num
-    while (i >= 0){
-        console.log(i)
-        i--
+function countDown (number) {
+    
+    while (number >= 0) {
+        console.log(number)
+        number--
     }
+
+    return number
 }
+
+
+
+
+
+
+
+
+
+
+
+// function writeCards(name, event){
+//     let emptyArray = []
+//     for (let i = 0; i < name.length; i++){
+//         emptyArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+//     }
+//     return emptyArray
+// }
+
+// function countDown(num){
+//     let i = num
+//     while (i >= 0){
+//         console.log(i)
+//         i--
+//     }
+// }
